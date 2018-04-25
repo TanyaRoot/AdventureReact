a974b428d426a43f62f78c1b7b30dc74558d9f5a
TanyaRoot <kofeneformat@gmail.com>
2018-04-25 19:24:45 +0300

    files update, first hard try

---
+++ src/Article.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Article() {
+  return {
+    <div>
+      <h3>titfffle</h3>
+      <section>Text</section>
+    </div>
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Article from './Article'
+
+
+function HelloWorld() {
+  return <h1>Helldfdfdfo lklkWorld123</h1>
+}
+render (<HelloWorld />, document.getElementById('container'))
+
+// render (<Article />, document.getElementById('container'))
