a4ccdd3da8563aa249e62fbf7a65d588efb17f6f
TanyaRoot <kofeneformat@gmail.com>
2018-05-03 09:37:12 +0300

    settings

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path')
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    watch: true,
+
+    //если вотчер не бумбум, то poll его внатури
+    watchOptions: {
+      poll: true
+    },
+    devServer: {
+        proxy: [{
+            path: '/api/',
+            target: 'http://localhost:3001'
+        }],
+        historyApiFallback: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js/,
+                loaders: ['babel-loader'],
+                include: path.join(__dirname, 'src')
+            },
+            {
+                test: /\.css/,
+                loaders: ['style-loader', 'css-loader'],
+            }
+        ]
+    }
+}
